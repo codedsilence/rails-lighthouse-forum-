c9eb5efeb22c6641551a0422b928ff11398b7497
codedsilence <patrick.sawtell@gmail.com>
2015-10-27 18:53:19 +0000

    posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,22 @@
 class PostsController < ApplicationController
 
 	def index
-		
+		@posts = [
+			{
+				title: "Superstar",
+				author: "Carly Rae Jepson",
+				text: <<-eos.gsub(/\s+/," ").strip
+					Jepson has been my music idol since I started writing songs back when I was 4.
+				eos
+			},				
+			{
+				title: "Basketball Idol",
+				author: "Steve Nash",
+				text: <<-eos.gsub(/\s+/, " ").strip
+					Dude had moves nobody had ever seen. Breaking ankles.
+				eos
+			},
+		]
 	end
 
 end
