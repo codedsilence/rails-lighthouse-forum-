a122e26849c265d49f766f0db22141cb4f73e44e
codedsilence <patrick.sawtell@gmail.com>
2015-10-27 20:22:06 +0000

    security -w- post_params

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,13 @@ LighthouseForum::Application.routes.draw do
 
   get 'posts', to: 'posts#index'
 
+  get 'posts/new', to: 'posts#new'
+
   get 'posts/:id', to: 'posts#show'
+
+  post 'posts', to: 'posts#create'
+
+
   
 
   # The priority is based upon order of creation: first created -> highest priority.
